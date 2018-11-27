aeb7d873794a505fad4676f6b8a743086da26832
Allan JEGU <prestasic10@ouest-france.fr>
2018-11-27 11:58:37 +0100

    Authentification de l'utilisateur

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * UserFixture constructor.
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $adminUser = new User();
+        $adminUser->setEmail('admin@gmail.com')
+            ->setPseudo('Admin')
+            ->setRoles(['ROLE_ADMIN']);
+
+        $adminUser->setPassword($this->passwordEncoder->encodePassword(
+            $adminUser,
+            '123456'
+        ));
+        $manager->persist($adminUser);
+
+        $manager->flush();
+    }
+}
